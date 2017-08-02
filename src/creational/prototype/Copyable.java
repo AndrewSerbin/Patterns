ab0c1ddf6f425fe0a30bf95d74b7ab433d5e4e06
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-02 20:16:05 +0300

    Added prototype.

---
+++ src/creational/prototype/Copyable.java
@@ -0,0 +1,6 @@
+package creational.prototype;
+
+interface Copyable {
+
+	Object copy();
+}
---
+++ src/creational/prototype/HumanFactory.java
@@ -0,0 +1,18 @@
+package creational.prototype;
+
+class HumanFactory {
+
+	Human prototype;
+
+	public HumanFactory(Human human) {
+		setPrototype(human);
+	}
+	
+	public void setPrototype(Human prototype) {
+		this.prototype = prototype;
+	}
+	
+	Human makeCopy() {
+		return (Human) prototype.copy();
+	}
+}
